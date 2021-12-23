ee4165b584816b6919e9b7fbce74b7f0f3efc403
Taavi Pahk <95106680+taavippp@users.noreply.github.com>
2021-12-23 13:57:55 +0200

    Add files via upload

---
+++ elekter.py
@@ -0,0 +1,53 @@
+import requests as rq
+import regex as re
+
+amt=10
+
+def multiple_min(iterable=[], amount=1):
+    temp=list(iterable)
+    results=[]
+    num=0
+    for a in range(amount):
+        num=min(temp)
+        results.append(num)
+        temp.remove(num)
+    return results
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+site=rq.get("http://digigrupp.com/internal/elekter/")
+text=site.text.split("\n")
+to_del=[]
+for i,val in enumerate(text):
+    val=remove_html_tags(val)
+    if len(val)<=1 or val.find("nbsp")!=-1 or val=="\r" or val.find("(")!=-1 or val.find("[")!=-1:
+        to_del.append(i)
+    val=val.replace("\r","")
+    text[i]=val
+for i in range(len(to_del)-1,-1,-1):
+    text.pop(to_del[i])
+
+counter=0
+price=[]
+hour=[]
+for val in text:
+    counter+=1
+    if len(hour)==24:
+        break
+    if val[0].isnumeric():
+        if counter%2==0:
+            hour.append(int(val))
+        else:
+            price.append(float(val))
+for i,val in enumerate(price):
+    if str(val).find(".0")!=-1:
+        price[i]=int(val)
+
+print("Järjekorras madalamaist kõrgeimani.")
+lowest=multiple_min(price,amt)
+for val in lowest:
+    hour_val=hour[price.index(val)]
+    print("Kell {}-{}: \t{} €".format(hour_val,hour_val+1,val))
